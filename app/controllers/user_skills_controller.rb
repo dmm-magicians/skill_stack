0a6beb3bc5817a1a3c1ea8bc34d90f89010a7511
EC2 Default User <or.06.012@gmail.com>
2022-03-19 20:26:37 +0000

    スキル登録画面作成

--- app/controllers/user_skills_controller.rb
+++ app/controllers/user_skills_controller.rb
@@ -2,22 +2,19 @@ class UserSkillsController < ApplicationController
 
   # POST /user_skills or /user_skills.json
   def create
+    
     @user_skill = UserSkill.new(user_skill_params)
-
-    respond_to do |format|
-      if @user_skill.save
-        format.html { redirect_to user_skill_url(@user_skill), notice: "User skill was successfully created." }
-        format.json { render :show, status: :created, location: @user_skill }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @user_skill.errors, status: :unprocessable_entity }
-      end
+    if @user_skill.save
+      redirect_to skills_edit_path, notice: "保存に成功しました"
+    else
+      redirect_to skill_edit_path
     end
+    
   end
 
   private
     # Only allow a list of trusted parameters through.
     def user_skill_params
-      params.require(:user_skill).permit(:int, :int)
+      params.require(:user_skill).permit(:user_id, :skill_id)
     end
 end

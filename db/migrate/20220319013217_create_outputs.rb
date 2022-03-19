80bb6f60b0b4445eef56f52c8c61e411cec152db
EC2 Default User <ec2-user@ip-172-31-45-180.ap-northeast-1.compute.internal>
2022-03-19 23:33:26 +0000

    マイグレーションファイルの修正。

--- db/migrate/20220319013217_create_outputs.rb
+++ db/migrate/20220319013217_create_outputs.rb
@@ -1,8 +1,8 @@
 class CreateOutputs < ActiveRecord::Migration[5.2]
   def change
     create_table :outputs do |t|
-      t.integer :user_id, null: false, default: ""
-      t.integer :skill_id, null: false, default: ""
+      t.integer :user_id, null: false
+      t.integer :skill_id, null: false
       t.string :title, null: false, default: ""
       t.text :text, null: false
       t.float :time, null: false, default: ""

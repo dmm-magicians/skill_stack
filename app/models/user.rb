dacdf79b08b15636b88859e4c8e2ab9f0d6969be
EC2 Default User <or.06.012@gmail.com>
2022-03-19 06:28:08 +0000

    add signin,signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # ゲストユーザーの作成
+  def self.guest
+    find_or_create_by!(email: "test@test.com", name: "ゲストユーザー") do |user|
+      user.password = SecureRandom.urlsafe_base64
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  devise_for :users
+
+  # ログイン関連(利用しない機能のルーティングは削除する)
+  devise_for :users, controllers: {
+  sessions:      "users/sessions",
+  passwords:     "users/passwords",
+  registrations: "users/registrations"
+  }
+  # simple Login
+  post "simple_login", to: "users#user_simple_login"
+
+
   resources :outputs
   resources :user_skills
   resources :skills
